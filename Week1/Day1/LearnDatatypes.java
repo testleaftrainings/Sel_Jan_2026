78d5a02989dadb7a06bf1ca055bdce414bb6ab39
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-01-03 13:14:25 +0530

    Add files via upload

---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,51 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		//Declaration
+		//dataType     variable           value
+		//int           age          =     20;
+		//1word - lowercase
+		//more than 1 word - camelCase
+		
+		//byte
+		byte age=26;
+		
+		//short
+		short ageOfPerson=128;
+        
+		//int
+		int amount=40000;
+		//long
+		long mobileNumber=6925411170L;
+		
+		float rate=5.25f;
+		
+		float rupee=1.23456789f;
+		//System.out.println(rupee);
+		
+		double dollar=1.23456789;
+		//System.out.println(dollar);
+		
+		//char
+		char logo='T';
+		
+		//boolean
+		boolean value=false;
+		
+		//String
+		String name="TestLeaf";
+		
+		//1Variable
+		System.out.println(name);
+		
+		//More than 1 variable in a single syso
+		System.out.println(name+" "+logo);
+		
+		//The company name is Testleaf
+		System.out.println("The company name is "+name);
+		
+	}
+
+}
